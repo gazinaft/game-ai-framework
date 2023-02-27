21c27026e0b41f781dc80fe7dc95c95ff06c7df7
gazinaft <gazinaft@gmail.com>
2023-02-28 00:25:03 +0200

    Blackboard done

---
+++ CoreEntities/Blackboard/Blackboard.cs
@@ -0,0 +1,6 @@
+namespace CoreEntities.Blackboard;
+
+public class Blackboard
+{
+    public List<BlackboardData> Entries = new List<BlackboardData>();
+}
---
+++ CoreEntities/Blackboard/BlackboardData.cs
@@ -0,0 +1,8 @@
+namespace CoreEntities.Blackboard;
+
+public class BlackboardData
+{
+    public string Key { get; set; }
+    public Type Type { get; set; }
+    public Object Value { get; set; }
+}
