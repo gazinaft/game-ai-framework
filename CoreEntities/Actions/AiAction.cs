4e2dcb8a4d69a3af644415f88bbd1d6ebab01774
gazinaft <gazinaft@gmail.com>
2023-05-27 16:36:37 +0300

    make IsComplete virtual

--- CoreEntities/Actions/AiAction.cs
+++ CoreEntities/Actions/AiAction.cs
@@ -14,7 +14,7 @@ public abstract class AiAction
         ExpireTime = StartExpireTime;
     }
     
-    public bool IsComplete { get; protected set; }
+    public virtual bool IsComplete { get; protected set; }
     public bool Interrupt { get; set; }
 
     public abstract void Start();
